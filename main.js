e00865928c871befbaaa7b73d46e4a2ed930b2f0
matheusvp0816 <matheus.vinicius.pereira29@escola.pr.gov.br>
2025-08-07 12:29:24 +0000

    12

--- main.js
+++ main.js
@@ -60,18 +60,21 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificasenha ();
+    classificasenha (alfabeto.length);
 }
 
-function classificasenha(){
+function classificasenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2 (tamanhoAlfabeto)
     forcaSenha.classList.remove ('fraca' , 'media' , 'forte');
-   if (tamanhoSenha > 11){
+   if (entropia > 57){
     forcaSenha.classList.add ('forte');
    }
-    else if (tamanhoSenha > 5 && tamanhoSenha <= 11){
+    else if (entropia > 35 && entropia <= 57){
         forcaSenha.classList.add ('media');
     }
-        else if (tamanhoSenha <= 5){
+        else if (entropia <= 35){
             forcaSenha.classList.add('fraca');
    }    
+   const valorEntropia = document.querySelector ('.entropia')
+valorEntropia.textContent = "um computador pode levar até" + Math.floor((2**entropia)/(100e6*60*60*24))+ "dias para descobrir essa senha"
 }
